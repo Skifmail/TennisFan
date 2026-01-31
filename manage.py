32f7543b6db8803fcff093939af1d91f9a5e7a66
Developer <dev@example.com>
2026-01-31 19:10:41 +0300

    Переведен файл manage.py

--- manage.py
+++ manage.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Командная утилита Django для административных задач."""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Запустить административные задачи."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Не удалось импортировать Django. Убедитесь, что он установлен "
+            "и доступен в переменной окружения PYTHONPATH. "
+            "Вы не забыли активировать виртуальное окружение?"
         ) from exc
     execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
     main()
+
